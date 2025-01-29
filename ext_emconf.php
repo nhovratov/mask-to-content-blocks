1fa6ee8a86f6374d28d2e8542b02ebc927107b43
Nikita Hovratov <nikita.h@live.de>
2025-01-29 21:39:04 +0100

    [TASK] Add ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Mask to Content Blocks',
+    'description' => 'One command to migrate Mask elements to Content Blocks',
+    'category' => 'be',
+    'author' => 'Nikita Hovratov',
+    'author_email' => 'entwicklung@nikita-hovratov.de',
+    'author_company' => '',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.2-13.4.99',
+            'mask' => '',
+            'content_blocks' => '',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
